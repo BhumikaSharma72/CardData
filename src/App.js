38f3c795209976a6db230a61aab61e6c3e42eb71
BhumikaSharma72 <bhumikafrc123@gmail.com>
2024-04-08 15:49:04 +0530

    bhumi first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // import { BrowserRouter } from "react-router-dom";
 // import Ecomerce from "./Ecomerce";
 
-import CmpA from "./CmpA";
+import CardData from "./CardData";
+//import CmpA from "./CmpA";
 
 // import { Navbar } from "react-bootstrap";
 function App() {
@@ -12,7 +13,8 @@ function App() {
 <Ecomerce></Ecomerce>
 <Navbar></Navbar>
 </BrowserRouter> */}
-<CmpA></CmpA>
+{/* <CmpA></CmpA> */}
+<CardData></CardData>
 </div>)
 }
 
